3da7f47fddd529a8647f88589e7bd02a5e03deeb
Michael <Mikeronomicon@gmail.com>
2015-12-07 15:06:32 -0800

    fixed route issue

--- server.js
+++ server.js
@@ -11,13 +11,17 @@ function compile(str, path) {
   return stylus(str).set('filename', path);
 }
 
-app.set('views', __dirname + 'server/views');
+app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser());
+
 app.use(stylus.middleware({
   src: __dirname + '/public',
   compile: compile
 }));
-app.use(express.static(__dirname + 'public'));
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('partials/:partialsPath', function(req, res) {
   res.render('partials/' + req.params.partialPath);
