a5239928706942f7d37af8fc102e5129546de390
Michael <Mikeronomicon@gmail.com>
2015-12-07 15:43:05 -0800

    server working, mongo being problematic

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
   stylus = require('stylus'),
   logger = require('morgan'),
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -15,22 +16,37 @@ app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser());
-
-app.use(stylus.middleware({
-  src: __dirname + '/public',
-  compile: compile
-}));
-
+app.use(stylus.middleware(
+  {
+    src: __dirname + '/public',
+    compile: compile
+  }
+));
 app.use(express.static(__dirname + '/public'));
 
-app.get('partials/:partialsPath', function(req, res) {
-  res.render('partials/' + req.params.partialPath);
-})
-
-app.get('*', function(req, res) {
-  res.render('index');
+mongoose.connect('mongodb://localhost/multivision');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+  console.log('multivision db opened');
+});
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+  mongoMessage = messageDoc.message;
 });
 
-var port = 3000;
+app.get('/partials/:partialPath', function(req, res) {
+    res.render('partials/' + req.params.partialPath);
+});
+
+app.get('*', function(req, res) {
+  res.render('index', {
+    mongoMessage: mongoMessage
+  });
+});
+
+var port = 3030;
 app.listen(port);
-console.log("listening on port " + port);
+console.log('Listening on port ' + port + '...');
