8a92cd629fa56a71a2f104823378aa373c9a74ed
Michael <Mikeronomicon@gmail.com>
2015-12-08 15:11:15 -0800

    fixed routing issue with new version of express, still working on getting mongo running right

--- server.js
+++ server.js
@@ -15,13 +15,14 @@ function compile(str, path) {
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 app.use(logger('dev'));
-app.use(bodyParser());
-app.use(stylus.middleware(
-  {
-    src: __dirname + '/public',
-    compile: compile
-  }
-));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+app.use(stylus.middleware({
+  src: __dirname + '/public',
+  compile: compile
+}));
 app.use(express.static(__dirname + '/public'));
 
 mongoose.connect('mongodb://localhost/multivision');
@@ -30,7 +31,9 @@ db.on('error', console.error.bind(console, 'connection error...'));
 db.once('open', function callback() {
   console.log('multivision db opened');
 });
-var messageSchema = mongoose.Schema({message: String});
+var messageSchema = mongoose.Schema({
+  message: String
+});
 var Message = mongoose.model('Message', messageSchema);
 var mongoMessage;
 Message.findOne().exec(function(err, messageDoc) {
@@ -38,7 +41,7 @@ Message.findOne().exec(function(err, messageDoc) {
 });
 
 app.get('/partials/:partialPath', function(req, res) {
-    res.render('partials/' + req.params.partialPath);
+  res.render('partials/' + req.params.partialPath);
 });
 
 app.get('*', function(req, res) {
