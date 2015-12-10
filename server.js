58f87e0b8b4d8ec48671aef517e7351868dc15f4
Michael <Mikeronomicon@gmail.com>
2015-12-10 14:49:08 -0800

    fixed issue with jade layout files, still trying to get server to work

--- server.js
+++ server.js
@@ -15,20 +15,18 @@ function compile(str, path) {
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-app.use(bodyParser.json());
-
+app.use(bodyParser());
 app.use(stylus.middleware({
   src: __dirname + '/public',
   compile: compile
 }));
-
 app.use(express.static(__dirname + '/public'));
 
-mongoose.connect('mongodb://localhost/multivision');
+if (env === 'development') {
+  mongoose.connect('mongodb://localhost/multivision');
+} else {
+  mongoose.connect('');
+}
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error...'));
 db.once('open', function callback() {
@@ -41,7 +39,6 @@ var messageSchema = mongoose.Schema({
 
 var Message = mongoose.model('Message', messageSchema);
 var mongoMessage;
-
 // Message.findOne().exec(function(err, messageDoc) {
 //   mongoMessage = messageDoc.message;
 // });
@@ -56,6 +53,6 @@ app.get('*', function(req, res) {
   });
 });
 
-var port = 3030;
+var port = process.env.PORT || 3030;
 app.listen(port);
 console.log('Listening on port ' + port + '...');
